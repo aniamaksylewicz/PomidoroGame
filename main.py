f74cf3c2be5d0508f9277d52217b28b8fa2be221
anna <anna.maksylewicz@gmail.com>
2024-08-27 12:56:15 +0200

    The final Pomidoro project.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import time
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,23 +11,88 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
-# ---------------------------- TIMER RESET ------------------------------- # 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+reps = 0
+timer = None
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomidoro")
-window.config(padx = 100, pady = 50, bg = GREEN)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width =200, height = 224, bg = GREEN, highlightthickness= 0 )
-tomato_img = PhotoImage(file = "tomato.png")  # specify photo image file name
-canvas.create_image(100, 112, image =tomato_img )
-canvas.create_text(100, 130, text="00:00",fill = "white",  font = (FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")  # specify photo image file name
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white",  font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+
+timer_label = Label(text="Timer", font=(FONT_NAME, 30, "bold"), bg=YELLOW, fg=GREEN)
+timer_label.grid(column=1, row=0)
+
+
+check_label = Label(font=(FONT_NAME, 15, "bold"), bg=YELLOW, fg=GREEN)
+check_label.grid(column=1, row=3)
+
+# ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_time():
+    window.after_cancel(timer)
+    check_label.config(text="")
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    global reps
+    reps = 0
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+    global timer
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_button_clicked()
+        mark = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            mark += "✔"
+        check_label.config(text=mark)
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start_button_clicked():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 2 != 0:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Short Break", fg=PINK)
+    elif reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Long Break", fg=GREEN)
+
+
+
+
+start_button = Button(text="Start", command=start_button_clicked)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_time)
+reset_button.grid(column=2, row=2)
+
+
 
 
 
